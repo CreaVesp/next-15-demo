1b4fae41002950bc3c1305d0ef8e9356c97e46d5
mpchelintsev <mpchelintsev@frontier.kz>
2025-02-06 17:38:11 +0500

    page templates and server actions split

---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Topic page",
+    description: "Selected Topic",
+    keywords: ['topic', 'page'],
+};
+
+export default function TopicShowPage() {
+    return <div>Topic Show Page</div>
+}
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,11 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Create Post",
+    description: "Post creation page",
+    keywords: ['post', 'create'],
+};
+
+export default function PostCreatePage() {
+    return <div>Post Create Page</div>
+}
