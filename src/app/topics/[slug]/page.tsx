acf564b97cb5500e408e75d8df604c55a6a57d6c
mpchelintsev <mpchelintsev@frontier.kz>
2025-02-10 20:26:53 +0500

    fetching url params for page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -6,6 +6,19 @@ export const metadata: Metadata = {
     keywords: ['topic', 'page'],
 };
 
-export default function TopicShowPage() {
-    return <div>Topic Show Page</div>
+interface TopicShowPageProps {
+    params: Promise<{slug: string}>
+}
+
+export default async function TopicShowPage({params}: TopicShowPageProps) {
+    const {slug} = await params
+
+    return <div className={'grid grid-cols-4 gap-4 p-4'}>
+        <div className={'col-span-3'}>
+            <h1 className={'text-2xl font-bold mb-2'}>
+                {slug}
+            </h1>
+        </div>
+        <div></div>
+    </div>
 }
