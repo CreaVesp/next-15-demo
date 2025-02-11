36d27999466804f2d23c87b6f53b241cd9845c2d
mpchelintsev <mpchelintsev@frontier.kz>
2025-02-11 19:43:12 +0500

    posts and related comments queries

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import type {Metadata} from "next";
 import PostCreateForm from "@/components/posts/PostCreateForm";
+import PostList from "@/components/posts/PostList";
+import {fetchPostsByTopicSlug} from "@/db/queries/posts";
 
 export const metadata: Metadata = {
     title: "Topic page",
@@ -16,9 +18,8 @@ export default async function TopicShowPage({params}: TopicShowPageProps) {
 
     return <div className={'grid grid-cols-4 gap-4 p-4'}>
         <div className={'col-span-3'}>
-            <h1 className={'text-2xl font-bold mb-2'}>
-                {slug}
-            </h1>
+            <h1 className={'text-2xl font-bold mb-2'}>{slug}</h1>
+            <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
         </div>
         <div>
             <PostCreateForm slug={slug}/>
--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import {fetchCommentsByPostId} from "@/db/queries/comments";
 import Link from "next/link";
 import PostShow from "@/components/posts/PostShow";
 import CommentList from "@/components/comments/CommentList";
@@ -5,23 +6,23 @@ import CommentCreateForm from "@/components/comments/CommentCreateForm";
 import paths from "@/paths";
 
 interface PostShowPageProps {
-  params: Promise<{
-    slug: string;
-    postId: string;
-  }>;
+    params: Promise<{
+        slug: string;
+        postId: string;
+    }>;
 }
 
-export default async function PostShowPage({ params }: PostShowPageProps) {
-  const { slug, postId } = await params;
+export default async function PostShowPage({params}: PostShowPageProps) {
+    const {slug, postId} = await params;
 
-  return (
-    <div className="space-y-3">
-      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {"< "}Back to {slug}
-      </Link>
-      {/* <PostShow /> */}
-      {/* <CommentCreateForm postId={postId} startOpen /> */}
-      {/* <CommentList comments={comments} /> */}
-    </div>
-  );
+    return (
+        <div className="space-y-3">
+            <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+                {"< "}Back to {slug}
+            </Link>
+            <PostShow postId={postId}/>
+            <CommentCreateForm postId={postId} startOpen/>
+            <CommentList fetchData={() => fetchCommentsByPostId(postId)}/>
+        </div>
+    );
 }
