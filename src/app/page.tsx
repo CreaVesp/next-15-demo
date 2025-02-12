3bf000bf618a3017a86bf69a9bd541c510b0040e
mpchelintsev <mpchelintsev@frontier.kz>
2025-02-12 13:40:21 +0500

    top posts on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import TopicCreateForm from "@/components/topics/TopicCreateForm";
 import TopicsList from "@/components/topics/TopicsList";
 import {Divider} from "@heroui/divider";
+import PostList from "@/components/posts/PostList";
+import {fetchTopPosts} from "@/db/queries/posts";
 
 export default function Home() {
     return <div className={'grid grid-cols-4 gap-4 p-4'}>
         <div className={'col-span-3'}>
             <h1 className={'text-xl m-2'}>Top Posts</h1>
+            <PostList fetchData={() => fetchTopPosts()}/>
         </div>
         <div className={'border shadow py-3 px-2 rounded-xl flex flex-col'}>
             <TopicCreateForm />
